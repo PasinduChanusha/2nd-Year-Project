202ec07ff59bf6b3ceb8959e06079e3bdec83b16
Pasindu Sooriyabandara <56555775+PasinduChanusha@users.noreply.github.com>
2021-05-14 19:20:50 +0530

    Add files via upload

---
+++ Malmi/HELA/Delivery/acceptdelivery.php
@@ -0,0 +1,16 @@
+<?php require_once("../add/ImConnect.php") ?>
+ <?php session_start(); ?>
+<?php 
+$ido = $_GET["oid"];
+$idp = $_GET["pid"];
+$sql = "UPDATE order_item SET order_status ='Delivered' WHERE o_id =$ido AND p_id=$idp" ;
+
+
+$result = mysqli_query($connection,$sql);
+if($result){
+header("Location: ../indexdelivery.php");
+
+}
+else
+	echo "Accepted Fail";
+?>
