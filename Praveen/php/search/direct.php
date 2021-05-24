f84750465e379ebdbee940ed6120203572f6fe23
Pasindu Sooriyabandara <56555775+PasinduChanusha@users.noreply.github.com>
2021-05-24 15:19:31 +0530

    Add files via upload

---
+++ Praveen/php/search/direct.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>skddddd</h1>
+    <?php
+    if(isset($_GET["id"])){
+        $spid = $_GET["id"];
+        echo $spid;
+    }
+    ?>
+    
+</body>
+</html>
---
+++ Praveen/php/search/inde.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <script text="text/javascript" src="php/search/livesearch.js"></script>
+</head>
+<body>
+<div>
+<input   placeholder="Search in..." size="40" onkeyup="showResult(this.value)">
+<div id="livesearch"></div>
+</div>
+
+</body>
+</html>
+ 
